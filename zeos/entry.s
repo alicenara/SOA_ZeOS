a335e7b16fc060a3c8dc6a231d92b7e3439e4226
Lucia Di Marco Lestar <lucia.di.marco@a5s102pc35.fib.upc.es>
2015-09-22 15:50:39 +0200

    solucio keyboard

--- zeos/entry.s
+++ zeos/entry.s
@@ -12,18 +12,12 @@
 # 6 "entry.S" 2
 # 1 "include/segment.h" 1
 # 7 "entry.S" 2
-# 58 "entry.S"
-      popl %ebx; popl %ecx; popl %edx; popl %esi; popl %edi; popl %ebp; popl %eax; popl %ds; popl %es; popl %fs; popl %gs
-# 72 "entry.S"
-      movb $0x20, %al; outb %al, $0x20;
-
-
-
+# 74 "entry.S"
 .globl keyboard_handler; .type keyboard_handler, @function; .align 0; keyboard_handler:
       pushl %gs; pushl %fs; pushl %es; pushl %ds; pushl %eax; pushl %ebp; pushl %edi; pushl %esi; pushl %edx; pushl %ecx; pushl %ebx; movl $0x18, %edx; movl %edx, %ds; movl %edx, %es
-     
+      movb $0x20, %al; outb %al, $0x20;
       call keyboard_routine
-     
+      popl %ebx; popl %ecx; popl %edx; popl %esi; popl %edi; popl %ebp; popl %eax; popl %ds; popl %es; popl %fs; popl %gs
       iret
 
 .globl system_call_handler; .type system_call_handler, @function; .align 0; system_call_handler:
@@ -35,8 +29,8 @@
       call *sys_call_table(, %eax, 0x04)
       jmp fin
 err:
-      movl $- -38, %eax
+      movl $-38, %eax
 fin:
       movl %eax, 0x18(%esp)
-     
+      popl %ebx; popl %ecx; popl %edx; popl %esi; popl %edi; popl %ebp; popl %eax; popl %ds; popl %es; popl %fs; popl %gs
       iret
